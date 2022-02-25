20cb7cb340b2a38cf4fc589b17812d619817c25e
Raele Pereira <raelepereira.nutri@gmail.com>
2022-02-25 19:50:23 -0300

    testando filter

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
-import data from './data/rickandmorty/rickandmorty.js';
 import {} from './data.js';
+import data from './data/rickandmorty/rickandmorty.js';
 
+const seletor = document.getElementById("filter-status");
+
+seletor.addEventListener("change", (event) => {
+  console.log(event.target.value);
+
+const filtroData = data.results.filter((item) => {
+  return item.status === event.target.value;
+});
+
+console.log(filtroData);
+});
 
 
 function showCards(data) {
@@ -13,7 +24,16 @@ function showCards(data) {
       <section class="container-name">
         <h3 class="name"><strong>${item.name}</strong></h3>
       </section>
+      
+      <section id="info-card"class="info">
+      <p class="list-item">Status: ${item.status}</p>
+      <p class="list-item">Gender: ${item.gender}</p>
+      <p class="list-item">Species: ${item.species}</p>
+      <p class="list-item">Appears in: ${item.episode.length} episodes</p>
+      <p class="list-item">Origin: ${item.origin.name}</p>
+    </section>
 
+  </section>
       
 
     </section>
