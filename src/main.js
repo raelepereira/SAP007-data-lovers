d0c55bdf0fdf01ca855c801f69c684c1619a2c7e
Raele Pereira <raelepereira.nutri@gmail.com>
2022-03-02 09:46:33 -0300

    refactor/css/add-filter

--- src/main.js
+++ src/main.js
@@ -1,22 +1,9 @@
-import {} from './data.js';
-import data from './data/rickandmorty/rickandmorty.js';
-
-const seletor = document.getElementById("filter-status");
-
-seletor.addEventListener("change", (event) => {
-  console.log(event.target.value);
-
-const filtroData = data.results.filter((item) => {
-  return item.status === event.target.value;
-});
-
-console.log(filtroData);
-});
+import {} from "./data.js";
+import dadosRickAndMorty from "./data/rickandmorty/rickandmorty.js";
 
 
 function showCards(data) {
   document.getElementById('card-container').innerHTML = data.map((item) => `
-
     <section id= "card-info" class="card">
         
       <img class="card-img" src="${item.image}">
@@ -32,15 +19,20 @@ function showCards(data) {
       <p class="list-item">Appears in: ${item.episode.length} episodes</p>
       <p class="list-item">Origin: ${item.origin.name}</p>
     </section>
-
   </section>
       
-
     </section>
-
   `).join("");
 }
 
-  
-  showCards(data.results);
+showCards(dadosRickAndMorty.results);
+
+const seletorGenero = document.getElementById("filter-gender"); // CONST COM O SELETOR
+
+seletorGenero.addEventListener("change", (event) => {
+  const filtroGenero = dadosRickAndMorty.results.filter((item) => {
+    return item.gender === event.target.value
+  });
+  console.log(filtroGenero);
+});
 
