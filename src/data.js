a18aba46a7c28312cf85251330cfcee7083248b8
Raele Pereira <raelepereira.nutri@gmail.com>
2022-03-04 17:31:08 -0300

    ajustando funcao de ordenar

--- src/data.js
+++ src/data.js
@@ -6,18 +6,11 @@
 //FUNÇÃO DE ORDERNAR A-Z e (Z-A < não funciona)
   export const ordering = (data, sort) => {
   
-
-    if (sort === "AZ") {
-      console.log("entrou no if", data.sort((a, b) => a.name < b.name ? -1 : 1)
-      )
-      return data.sort((a, b) => a.name < b.name ? -1 : 1)
-      
+     g if (sort === "AZ") {  
+      return data.sort((a, b) => a.name < b.name ? -1 : 1)     
     } else {
-      console.log("entrou no else", data.sort((a, b) => a.name > b.name ? -1 : 1))
       return data.sort((a, b) => a.name > b.name ? -1 : 1)
-
-    } 
-    
+   } 
   };
 
 //função de procurar pelo nome (input)
